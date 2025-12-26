f35f78884764c03bbdf306079f487e52ff3a30f3
strbeni <amityadv747@gmail.com>
2025-12-26 19:17:05 +0530

    Added Click Event

--- src/Layout.js
+++ src/Layout.js
@@ -2,20 +2,27 @@ import React from 'react';
 // import { Helmet } from 'react-helmet';
 import Navbar from './app/Components/Navbar/Navbar';
 import Footer from './app/Components/Footer/Footer';
+import ClickSpark from './app/Components/ui/ClickSpark';
 // import Navbar from './components/Navbar/Navbar';
 // import Footer from './components/Footer/Footer';
 // import Connect from './components/connect/connect';
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      
-      
-      < Navbar />
-      {children}
-      {/* <Connect /> */}
-      <Footer/>
-    </div>
+    <ClickSpark
+      sparkColor='#fff'
+      sparkSize={10}
+      sparkRadius={15}
+      sparkCount={8}
+      duration={400}
+    >
+      <div>
+        < Navbar />
+        {children}
+        {/* <Connect /> */}
+        <Footer />
+      </div>
+    </ClickSpark>
   );
 };
 
